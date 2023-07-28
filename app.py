eccc3484b6e13943fae2fe75c540b31d9fcbffe1
TopDevSteven <cheerman00526@gmail.com>
2023-07-28 07:48:38 -0700

    updated with db and app.py

--- app.py
+++ app.py
@@ -5,13 +5,13 @@ from fastapi.middleware.cors import CORSMiddleware
 from langchain.prompts import PromptTemplate
 from dotenv import load_dotenv
 import openai
-openai.api_key = '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'
+
 import os
 
 load_dotenv()
 
 openai_key = os.getenv("OPENAI_API_KEY")
-
+openai.api_key = os.getenv("OPENAI_API_KEY")
 llm = ChatOpenAI(model_name='gpt-4', openai_api_key=openai_key, temperature=0.4)
 current_path = os.path.dirname(__file__)
 dburi = os.path.join('sqlite:///' + current_path,
@@ -56,10 +56,11 @@ Else
             `Type2`,
             ...
         }
+        I can help you to find Size, Link, Vendor, Diameter, SKU , etc on above tools.
         Here `Type1` and `Type2` are the unique from column `Type` like "Clamping Heads".
 
-    Else, the question is to ask type included in coulmn `Type` like "Clamping Heads", select column `Tool_name`, `Minumum Diameter`,`Maximum Diameter`, `Range`, `Vendor`, `SKU` and use the following format.
-        " `meTool_na` by `Vendor`, from `Minumum Diameter` to `Maximum Diameter` with step `Range` available. SKU is `SKU` "
+    Else, the question is to ask type included in coulmn `Type` like "Clamping Heads", select column `Tool_name`, `Minimum Diameter`,`Maximum Diameter`, `Range`, `Vendor`, `SKU` and use the following format.
+        " `Tool_name` by `Vendor`, from `Minimum Diameter` to `Maximum Diameter` with step `Range` available. SKU is `SKU` "
         Here, `Tool_name` must be unique not the same.
         And `Maximum Diameter` should be maximum value of diameter in `Diameter`.
         And `Minimum Diameter` should be minimum value of diameter in `Diameter`.
@@ -68,8 +69,8 @@ Else
 
         Output one by one in a line.
     
-    Else, the question is to ask type included in coulmn `Tool_name`, select column `Minumum Diameter`,`Maximum Diameter`, `Range`, `Vendor`,`SKU` and use the following format.
-    " `Tool_name` by `Vendor`, from `Minumum Diameter` to `Maximum Diameter` with step `Range` available. SKU is `SKU` "
+    Else, the question is to ask toolname included in coulmn `Tool_name`, select column `Minimum Diameter`,`Maximum Diameter`, `Range`, `Vendor`,`SKU` and use the following format.
+    " `Tool_name` by `Vendor`, from `Minimum Diameter` to `Maximum Diameter` with step `Range` available. SKU is `SKU` "
         Here, `Tool_name` must be unique not the same.
         And `Maximum Diameter` should be maximum value of diameter in `Diameter`.
         And `Minimum Diameter` should be minimum value of diameter in `Diameter`.
@@ -78,8 +79,8 @@ Else
         `Maximum Diameter`, `Minimum Diameter`, `Range` must be included.
         Output one by one in a line.
     
-    Else, the question is to ask type included in coulmn `Size` like "Type 32L", select column `Tool_name`, `Minumum Diameter`,`Maximum Diameter`, `Range`, `Vendor`,`SKU` and use the following format.
-    " `Tool_name` by `Vendor`, from `Minumum Diameter` to `Maximum Diameter` with step `Range` available. SKU is `SKU` "
+    Else, the question is to ask size included in coulmn `Size` like "32L", select column `Tool_name`, `Minimum Diameter`,`Maximum Diameter`, `Range`, `Vendor`,`SKU` and use the following format.
+    " `Tool_name` by `Vendor`, from `Minimum Diameter` to `Maximum Diameter` with step `Range` available. SKU is `SKU` "
         Here, `Tool_name` must be unique not the same.
         And `Maximum Diameter` should be maximum value of diameter in `Diameter`.
         And `Minimum Diameter` should be minimum value of diameter in `Diameter`.
@@ -88,11 +89,14 @@ Else
         `Maximum Diameter`, `Minimum Diameter`, `Range` must be included.
         Output one by one in a line.
 
-    Else, the question is to ask type included in coulmn `Diameter` like "diameter 42", select column `Tool_name`, `Vendor`, `SKU` and use the following format.
+    Else, the question is to ask diameter included in coulmn `Diameter` like "diameter 42", select column `Tool_name`, `Vendor`, `SKU` and use the following format.
     " `Tool_name` by `Vendor` available. "
         Here, `Tool_name` must be unique not the same.
         For example, " Clamping Heads - Type 32L - Round - L(smooth) by DT GROUP available. sku is `SKU`."
         Output one by one in a line.
+    Else, the question is to ask link included in coulmn `Link` like "...link of...", select coulmn `Link` and use the following format.
+    "https://Hectool/`Link`"
+    For example, "https://Hectool/clamping-heads-type-32l-round-l-smooth"
 Else, there is no answer to the question, must output the following sentence. "Ooops" 
 """
 
